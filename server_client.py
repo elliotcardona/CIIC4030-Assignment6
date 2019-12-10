d147d0a5da1c3c79a0dd944a259855b5b34a959e
angeldburgos <angel.burgos11@upr.edu>
2019-12-10 16:09:59 -0400

    added to server_client.py, can now leave client and re enter

--- server_client.py
+++ server_client.py
@@ -1,13 +1,71 @@
 import socket
+import sys
 import threading
 
+
+sPort = 10000
+cPort = 10000
+serverSocketFamily = socket.AF_INET
+serverSocketType = socket.SOCK_STREAM
+clientSocketFamily = socket.AF_INET
+clientSocketType = socket.SOCK_STREAM
+
+
+def setServerPort(p):
+    global sPort
+    sPort = p
+
+def setClientPort(p):
+    global cPort
+    cPort = p
+
+def setServerSocketFamily(sF):
+    global serverSocketFamily
+    serverSocketFamily = socket.sF
+
+def setServerSocketType(sT):
+    global serverSocketType
+    serverSocketType = sT
+
+def setClientSocketFamily(sF):
+    global clientSocketFamily
+    clientSocketFamily = sF
+
+def setClientSocketType(sT):
+    global clientSocketType
+    clientSocketType = sT
+
+def getServerPort():
+    global sPort
+    return sPort
+
+def getClientPort():
+    global cPort
+    return cPort
+
+def getServerSocketFamily():
+    global serverSocketFamily
+    return serverSocketFamily
+
+def getServerSocketType():
+    global serverSocketType
+    return serverSocketType
+
+def getClientSocketFamily():
+    global clientSocketFamily
+    return clientSocketFamily
+
+def getClientSocketType():
+    global clientSocketType
+    return clientSocketType
+
 class Server:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(serverSocketFamily, serverSocketType)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     connections = []
 
     def __init__(self):
-        self.sock.bind(('0.0.0.0',10000))
+        self.sock.bind(('0.0.0.0',sPort))
         self.sock.listen(1)
         print("Server running...", self.sock.getsockname())
     def run(self):
@@ -20,27 +78,39 @@ class Server:
             print(str(a[0])+':'+str(a[1])+ " connected")
 
     def handler(self, c, a):
+        T = False
         while True:
             data = c.recv(1024)
             for connection in self.connections:
-                connection.send(bytes(data))
-            if not data:
+                if str(data, 'utf-8') == "END_SESSION" and connection == c:
+                    T = True
+                    break
+                if not connection == c:
+                    msg = "(" + str(a[0]) + ":" + str(a[1]) + ")" + "=> " + str(data, 'utf-8')
+                    connection.send(bytes(msg, 'utf-8'))
+            if not data or T:
                 print(str(a[0])+':'+str(a[1]), " disconnected")
                 self.connections.remove(c)
                 c.close()
                 break
 
 class Client:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    sock = socket.socket(clientSocketFamily, clientSocketType)
     def sendMsg(self):
         while True:
-            self.sock.send(bytes(input(""), 'utf-8'))
+            t = input("")
+            self.sock.send(bytes(t, 'utf-8'))
+            print(t)
+            if t == 'END_SESSION':
+                self.sock.close()
+                break
+
 
     def __init__(self, address):
-
-        self.sock.connect((address, 10000))
+        self.sock = socket.socket(clientSocketFamily, clientSocketType)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.connect((address, cPort))
 
         iThread = threading.Thread(target=self.sendMsg)
         iThread.daemon = True
@@ -48,8 +118,6 @@ class Client:
         print('Connected:')
         while True:
             data = self.sock.recv(1024)
-            if(str(data, 'utf-8') == 'END_SESSION'):
-                break
             if not data:
                 break
             print(str(data, 'utf-8'))
@@ -57,7 +125,18 @@ class Client:
 def execute(t):
     print("Trying to connect...")
     if(t == 'client'):
-        client = Client('127.0.0.1')
+        try:
+            client = Client('0.0.0.0')
+            #127.0.0.1
+        except KeyboardInterrupt:
+            pass
+        except:
+            print("Could not establish connection with server! Make sure server is running.")
     else:
-        server = Server()
-        server.run()
+        try:
+            server = Server()
+            server.run()
+        except KeyboardInterrupt:
+            pass
+        except:
+            print("Could not start up the server!")
