f596ebb124b6b5ff9adc37d5f9c945f047658765
Elliot J. Cardona Pena <elliot.cardona@upr.edu>
2019-12-09 20:03:16 -0400

    Merge branch 'secondary' of /Users/batpc/PycharmProjects/CIIC4030-Assignment6 with conflicts.

---
+++ Parser.py
@@ -0,0 +1,23 @@
+import sys
+import ply.yacc as yacc
+from Lexer import tokens
+import server_client
+
+def p_start_server(p):
+    'start : START_SERVER'
+    print(p[1])
+    server_client.execute('server')
+
+def p_create_client_noIP(p):
+    'start : START_CLIENT'
+    server_client.execute('client')
+
+def p_done(p):
+    'start : FINISH'
+    sys.exit(0)
+
+
+def p_error(p):
+    print("Syntax error in input!")
+
+parser = yacc.yacc()
