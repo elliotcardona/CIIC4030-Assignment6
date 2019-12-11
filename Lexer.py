314744c257081c7dc86785be61d0d241dc945f67
angeldburgos <31297282+angeldburgos@users.noreply.github.com>
2019-12-11 13:05:05 -0400

    deleted unnecessary code

--- Lexer.py
+++ Lexer.py
@@ -37,7 +37,6 @@ def t_NUMBER(t):
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value, 'ID')  # Check for reserved words
-    print ("ID: '%s'" % t.value)
     return t
 
 
@@ -46,24 +45,3 @@ def t_error(t):
  t.lexer.skip(1)
 
 lexer = lex.lex()
-
-#testing lexer
-#data1 = r'ssd'
-#data2 = r'START_SERVER'
-#data3 = r'END_SERVER'
-
-#print ("Data: '%s'" % data0)
-#print ("Data: '%s'" % data1)
-#print ("Data: '%s'" % data2)
-#print ("Data: '%s'" % data3)
-
-
-#lexer.input(data0)
-#lexer.input(data1)
-#lexer.input(data2)
-#lexer.input(data3)
-
-#while True:
-#   tok = lexer.token()
-#   if not tok: break
-#s   print (tok)
